37aa067febb1b5ebf025e09aa941865dec1cf6d2
David Fuchssteiner <david.fuchssteiner@gmx.at>
2023-01-04 16:51:40 +0100

    fixed small bug

--- src/naive_ramanujan.cpp
+++ src/naive_ramanujan.cpp
@@ -7,11 +7,12 @@ int cube(int n) {
     return n * n * n;
 }
 
-std::vector<int> generate_ramanujan_numbers(unsigned int limit) {
+std::vector<int> generate_ramanujan_numbers(unsigned int n) {
     std::set<int> ramanujan_candidates;
     std::vector<int> ramanujan_numbers;
-    for (size_t i = 0; i < limit; i++) {
-        for (size_t j = i + 1; j < limit; j++) {
+    auto limit = cube(n);
+    for (size_t i = 0; cube(i) <= n; i++) {
+        for (size_t j = i + 1; cube(i) + cube(j) <= n; j++) {
             auto num = cube(i) + cube(j);
             auto res = ramanujan_candidates.insert(num);
             if (!res.second) {
