9358bbe65e2c6404989222719544eb424bb4e9f0
David Fuchssteiner <david.fuchssteiner@gmx.at>
2023-01-04 17:25:30 +0100

    up

--- src/naive_ramanujan.cpp
+++ src/naive_ramanujan.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <utility>
 #include <algorithm>
 
 int cube(int n) {
     return n * n * n;
 }
 
-std::vector<int> generate_ramanujan_numbers(unsigned int n) {
+using result_type = std::pair<std::set<int>, std::vector<int>>;
+
+result_type generate_ramanujan_numbers(unsigned int n) {
     std::set<int> ramanujan_candidates;
     std::vector<int> ramanujan_numbers;
-    auto limit = cube(n);
+
     for (size_t i = 0; cube(i) <= n; i++) {
         for (size_t j = i + 1; cube(i) + cube(j) <= n; j++) {
             auto num = cube(i) + cube(j);
@@ -20,13 +23,14 @@ std::vector<int> generate_ramanujan_numbers(unsigned int n) {
             }
         }
     }
-    return ramanujan_numbers;
+    return {ramanujan_candidates, ramanujan_numbers};
 }
 
 int main(int argc, char *argv[]) {
     // parse argument for limit N
     auto N = std::stol(argv[1], nullptr, 10);
-    auto ramanujans = generate_ramanujan_numbers(N);
+    auto [candidates, ramanujans] = generate_ramanujan_numbers(N);
     std::cout << "Ramanujan numbers up to " << N << ": " << ramanujans.size() << std::endl;
+    std::cout << "Memory usage: >=" << candidates.size() * sizeof(int) + ramanujans.size() * sizeof(int);
     return 0;
 }
