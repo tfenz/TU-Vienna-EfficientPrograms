010851f4c84687871a94b1ad4e34cac2a8f5fbee
David Fuchssteiner <david.fuchssteiner@gmx.at>
2023-01-22 18:03:22 +0100

    up

--- reference_programs/ramanujan.c
+++ reference_programs/ramanujan.c
@@ -84,7 +84,8 @@ int main(int argc, char **argv)
         occupation++;
       }
     }
-  printf("%ld Ramanujan numbers up to %ld, checksum=%ld\noccupation=%ld, size=%ld\n",count,n,checksum,occupation,table_size);
+  printff("occupation=%ld, size=%ld\n",occupation,table_size);
+  printf("%ld Ramanujan numbers up to %ld, checksum=%ld\n",count,n,checksum);
   printf("Memory usage: >=%ld\n",table_size*sizeof(struct node*)+occupation*sizeof(struct node));
   return 0;
 
